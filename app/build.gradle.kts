1a0da84c136d59089370952ff3a0b13b946e066a
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2024-05-04 02:21:54 +0800

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.pyoncord.xposed"
         minSdk = 24
         targetSdk = 33
-        versionCode = 201
-        versionName = "0.2.1"
+        versionCode = 202
+        versionName = "0.2.2"
     }
 
     buildTypes {
