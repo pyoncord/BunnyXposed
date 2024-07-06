a60e902e71166ae9f9b4ae05e076b18e13c4d637
Palm <palmpasuthorn@gmail.com>
2024-07-06 11:51:00 +0700

    fix+chore: hook JNI wrapper methods & bump version (#4)
    related issue: https://github.com/pyoncord/Bunny/issues/17
    
    * fix: hook a similar method for loading JS, fixes armv7
    
    * chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.pyoncord.xposed"
         minSdk = 24
         targetSdk = 33
-        versionCode = 202
-        versionName = "0.2.2"
+        versionCode = 203
+        versionName = "0.2.3"
     }
 
     buildTypes {
